656780c719c8adef9031573064d5de5745e228d3
Bertocco Thomas-Killian <bertocco.tk@gmail.com>
2019-08-25 23:19:08 +0800

    Update server.js
    route GET time

--- Hello World/server.js
+++ Hello World/server.js
@@ -1,16 +1,21 @@
 //server.js
 const express = require('express'),
-      server = express();
+  server = express();
 
 //setting the port.
 server.set('port', process.env.PORT || 3000);
 
 //Adding routes
-server.get('/',(request,response)=>{
- response.sendFile(__dirname + '/index.html');
+server.get('/', (request, response) => {
+  response.sendFile(__dirname + '/index.html');
+});
+
+server.get('/time', (request, response) => {
+  var date = new Date();
+  response.send(date.getFullYear() + ":" + (date.getMonth() + 1) + ":" + date.getDate() + " | " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
 });
 
 //Binding to localhost://3000
-server.listen(3000,()=>{
- console.log('Express server started at port 3000');
-});
+server.listen(3000, () => {
+  console.log('Express server started at port 3000');
+})
